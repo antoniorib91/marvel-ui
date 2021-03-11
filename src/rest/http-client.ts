ea0edc78c4ce398366f10151eb03be84bd28ca29
Antônio Ribeiro <antonio.ribeiro@softdesign.com.br>
2021-03-11 13:54:31 -0300

    feature/mvui-04 - Create HttpClient and marvel rest service classes. Mmade pagination service

--- src/rest/http-client.ts
+++ src/rest/http-client.ts
@@ -1,5 +1,12 @@
-export class HttpClient {
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-  // private instance: AxiosIntance
+const defaultURL = process.env.REACT_APP_API;
+class HttpClient {
+  private static axios: AxiosInstance = axios.create({ baseURL: defaultURL });
 
+  public static get<T>(url: string): Promise<AxiosResponse<T>> {
+    return this.axios.get<T>(url);
+  }
 }
+
+export default HttpClient;
